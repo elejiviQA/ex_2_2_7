5003ca924b0a4cfceb8596445992ed1b15b769ea
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-11-13 05:18:30 +0500

    create tests for roundTips method

--- src/test/java/TipServiceTest.java
+++ src/test/java/TipServiceTest.java
@@ -1,9 +1,19 @@
 import lombok.extern.java.Log;
+import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.ot5usk.Tester;
+import org.ot5usk.TipService;
+
+import java.math.BigDecimal;
 
 @Log
 public class TipServiceTest {
+
+    private final TipService tipService = new TipService();
 
     public void log(Tester helper, String requirement) {
         AssertionError assertionError = null;
@@ -14,11 +24,35 @@ public class TipServiceTest {
             assertionError = aErr;
         } finally {
             if (assertionError == null) {
-                log.info("[TEST WAS SUCCESSFUL]: " + "\"" + requirement + "\" : " + stackTraceElements[2] + "\n");
+                log.info("[TEST WAS SUCCESSFUL]: " + "\"" + requirement + "\"\n: " + stackTraceElements[2] + "\n");
             } else {
-                log.warning("[TEST WAS FAILED]: " + "\"" + requirement + "\" : " + stackTraceElements[2] + "\n");
+                log.warning("[TEST WAS FAILED]: " + "\"" + requirement + "\"\n: " + stackTraceElements[2] + "\n");
                 throw assertionError;
             }
         }
+    }
+
+    public BigDecimal getExpectedAmount(BigDecimal amount, BigDecimal multiplier) {
+        return amount.multiply(multiplier);
+    }
+
+    @DisplayName("Purchase amount of a thousand rubles or more")
+    @ParameterizedTest
+    @CsvSource({
+            "1000, 1.05",
+            "2000, 1.05",
+    })
+    public void testRoundTips_WithAmountOfThousandOrMore_AmountShouldIncreasedByFivePercent(BigDecimal amount, BigDecimal multiplier) {
+        log((requirement) -> assertEquals(getExpectedAmount(amount, multiplier), tipService.roundTips(amount)), "The purchase amount must be five percent more");
+    }
+
+    @DisplayName("The purchase amount is less than a thousand rubles")
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1.1",
+            "999, 1.1",
+    })
+    public void testRoundTips_WithAmountOfThousandOrMore_AmountShouldIncreasedByTenPercent(BigDecimal amount, BigDecimal multiplier) {
+        log((requirement) -> assertEquals(getExpectedAmount(amount, multiplier), tipService.roundTips(amount)), "The purchase amount must be ten percent more");
     }
 }
