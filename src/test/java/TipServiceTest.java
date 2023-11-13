2db02effb75c614320b2f92f7ff9e2f85057044f
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-11-13 16:44:57 +0500

    naming

--- src/test/java/TipServiceTest.java
+++ src/test/java/TipServiceTest.java
@@ -22,42 +22,41 @@ public class TipServiceTest {
         return amount.multiply(multiplier);
     }
 
-    @DisplayName("Total purchase amount from positive args")
+    @DisplayName("Amount from positive args")
     @Description("If the amount is <1000, then 10% is added to it, otherwise 5%")
     @ParameterizedTest(name = "{2} tip is added to the amount of {0} rubles")
     @CsvFileSource(resources = "/testData.csv")
-    public void testRoundTipsWithPositiveAmountValues(BigDecimal amount, BigDecimal multiplier, String percent) {
+    public void withPositiveSource(BigDecimal amount, BigDecimal multiplier, String percent) {
         assertEquals(getExpectedAmount(amount, multiplier), tipService.roundTips(amount), "The purchase amount must been " + percent + " more");
     }
 
-    @DisplayName("Total purchase amount from zero and less zero")
+    @DisplayName("Amount from zero and less zero")
     @Description("Test calculate")
     @ParameterizedTest(name = "If {0}, then {1}")
     @CsvSource({"0, 0", "-1, -1.1"})
-    public void testRoundTipsWithObjectionableAmountValuesAlongTheLowerBorder(BigDecimal amount, BigDecimal result) {
+    public void withObjectionableSource(BigDecimal amount, BigDecimal result) {
         assertEquals(result.intValue(), tipService.roundTips(amount).intValue(), "Must been " + result);
     }
 
     @Disabled
-    @DisplayName("Total purchase amount from negative args")
+    @DisplayName("Amount from negative args")
     @Description("Test catching ex")
     @ParameterizedTest(name = "If purchase amount value is {0}")
-    @MethodSource("negativeAmountValues")
-    public void testRoundTipsWithNegativeAmountValuesMustThrowException(BigDecimal negativeAmountValue) {
+    @MethodSource("negativeArgs")
+    public void withNegativeSourceMustBeenEx(BigDecimal negativeAmountValue) {
         assertThrows(RuntimeException.class, () -> tipService.roundTips(negativeAmountValue), "Must throw ex");
     }
 
-
     @Disabled
-    @DisplayName("Total purchase amount from negative args")
+    @DisplayName("Amount from negative args")
     @Description("Test nullable")
     @ParameterizedTest(name = "If purchase amount value is {0}")
-    @MethodSource("negativeAmountValues")
-    public void testRoundTipsWithNegativeAmountValuesMustGetZero(BigDecimal negativeAmountValue) {
+    @MethodSource("negativeArgs")
+    public void withNullableSourceMustBeenZero(BigDecimal negativeAmountValue) {
         assertEquals(0, tipService.roundTips(negativeAmountValue).intValue(), "Must been 0");
     }
 
-    static Stream<BigDecimal> negativeAmountValues() {
+    static Stream<BigDecimal> negativeArgs() {
         return Stream.of(null, BigDecimal.valueOf(-1));
     }
 }
